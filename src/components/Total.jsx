42a6d82c160dbe4b5ed0d8666f5c782b705a7280
Cuellar Dev <afca133@gmail.com>
2022-09-17 13:23:12 -0500

    proyecto finalizado con useMemo

--- src/components/Total.jsx
+++ src/components/Total.jsx
@@ -1,12 +1,47 @@
+import { useCallback, useRef, useMemo } from "react";
 import useCotizador from "../hooks/useCotizador";
+import { MARCAS, PLANES } from "../constans";
 
 const Total = () => {
-  const { total } = useCotizador();
+  const { total, datos } = useCotizador();
+  const { marca, year, plan } = datos;
+
+  const [nombreMarca] = useMemo(
+    () => MARCAS.filter((m) => m.id === Number(marca)),
+    [total]
+  );
+
+  const [nombrePlan] = useMemo(
+    () => PLANES.filter((p) => p.id === Number(plan)),
+    [total]
+  );
+
+  const yearRef = useRef(year);
 
   if (total === 0) {
     return null;
   }
-  return <div>{total}</div>;
+  return (
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
+      <p className="my-2">
+        <span className="font-bold">Marca: </span>
+        {nombreMarca.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {nombrePlan.nombre}
+      </p>
+      <p className="my-2">
+        <span className="font-bold">Año del auto: </span>
+        {yearRef.current}
+      </p>
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total cotización: </span>
+        {total}
+      </p>
+    </div>
+  );
 };
 
 export default Total;
